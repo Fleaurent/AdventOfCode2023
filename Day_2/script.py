5119c503936c7fb25e7c4d3357f1ad6f219e0c51
Florens Fraidling <florens.fraidling@emft.fraunhofer.de>
2023-12-24 23:40:55 +0100

    add day_2 part 2

--- Day_2/script.py
+++ Day_2/script.py
@@ -18,14 +18,39 @@ def get_input(filepath: Path) -> str:
         data = f.read().strip()  
     return data
 
-def parse_line(line: str) -> dict:
+def parse_line_part_1(line: str) -> bool:
+    """
+    parse line '3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green'
+    check if valid game i.e. if number of cubes per color is smaller than in bag_dict
+    """
+    bag_dict = {'red': 12, 'green': 13, 'blue': 14}
+    
+    # remove game number
+    game_str = line.split(':')[1].strip()        
+
+    for game_set in game_str.split(';'):
+        # run through each game set
+        for game_set_cubes in game_set.strip().split(','):
+            # check each cube if valid
+            number, color = game_set_cubes.strip().split(' ')            
+            
+            if(bag_dict[color] < int(number)):
+                return False
+    
+    return True
+
+def parse_line_part_2(line: str) -> dict:
     """ 
     parse line '3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green'
     find largest number of each color 
     return dict
     """
     game_dict = {'red': 0, 'blue': 0, 'green': 0}
-    for game in line.split(';'):
+    
+    # remove game number
+    game_str = line.split(':')[1].strip()
+    
+    for game in game_str.split(';'):
         
         for round in game.strip().split(','):
             # extract number and color: add to dict
@@ -38,25 +63,15 @@ def parse_line(line: str) -> dict:
 
 
 def part_1(data: str) -> int:
-    bag_dict = {'red': 12, 'green': 13, 'blue': 14}
+    """
+    sum of ids of valid games
+    """
     sum_ids = 0
     
     for index, game in enumerate(data.strip().split('\n')):
         # run through each game
-        game_valid = True
-        
-        game_str = game.split(':')[1].strip()
-        
-        for game_set in game_str.split(';'):
-            # run through each game set
-            for game_set_cubes in game_set.strip().split(','):
-                # check each cube if valid
-                number, color = game_set_cubes.strip().split(' ')            
-                
-                if(bag_dict[color] < int(number)):
-                    game_valid = False
-                    break
-                
+        game_valid = parse_line_part_1(game)
+
         if(game_valid):
             sum_ids += (index + 1)          
 
@@ -64,28 +79,29 @@ def part_1(data: str) -> int:
 
 def part_2(data: str) -> int:
     """
-    TEST
+    sum of power of maximum number of cubes per color
     """
-    for index, game in enumerate(data.strip().split('\n')):
-        # run through each game
-        game_str = game.split(':')[1].strip()
-        
-        game_dict = parse_line(game_str)       
-        print(game_dict)
+    power = 0
 
-    return 0
+    for game in data.strip().split('\n'):
+        # run through each game        
+        game_dict = parse_line_part_2(game)     
+        
+        power_i = game_dict['red'] * game_dict['blue'] * game_dict['green']
+        power += power_i
+
+    return power
 
 if __name__ == '__main__':
-    print(INPUT_FILE)
-
     print(EXAMPLE_INPUT)
+    
+    print(INPUT_FILE)
     input_str = get_input(INPUT_FILE)
-    # print(input_str)
 
     # Part 1
-    #print(part_1(EXAMPLE_INPUT))
-    #print(part_1(input_str))
+    print(part_1(EXAMPLE_INPUT))
+    print(part_1(input_str))
 
     # Part 2
     print(part_2(EXAMPLE_INPUT))
-    #print(part_2(data))
+    print(part_2(input_str))
